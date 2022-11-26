bd1d46c882a60eccc0efb71b9440e5190721a166
ashusatp <92241390+ashusatp@users.noreply.github.com>
2022-11-27 01:08:02 +0530

    defaul tValues

---
+++ lecture5/defaultValues.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract defaultValues{
+
+    uint public x;  // 0
+    bool public y;  // false
+    address public z; //address: 0x0000000000000000000000000000000000000000
+    bytes32 public a; //bytes32: 0x0000000000000000000000000000000000000000000000000000000000000000
+    string public str; // empty string
+  
+}
